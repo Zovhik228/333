84cdf5aeebe02cebe865c0c147c6728be52cf0df
danil <danil@DESKTOP-AHFQMQ9>
2025-02-14 00:41:49 +0500

    Разработанные контексты данных. Фамилия

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,23 @@
+﻿using _333.Classes.Common;
+using _333.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _333.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+        public MessagesContext()
+        {
+            Database.EnsureCreated();
+            Messages.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,23 @@
+﻿using _333.Classes.Common;
+using _333.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _333.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
