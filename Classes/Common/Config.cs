11d63adfbed00fe348da27a584c872c0fe0cf09a
danil <danil@DESKTOP-AHFQMQ9>
2025-02-14 00:27:43 +0500

    Разработанные общие классы. Фамилия».

---
+++ Classes/Common/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _333.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config =
+            "server=(localdb)\\localhost;Trusted_Connection=No;database=VPat;user=sa;pwd=Asdfg123;";
+    }
+}
