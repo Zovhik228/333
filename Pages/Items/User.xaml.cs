d916cc5592150c4d5543c1b7650a8f56c7b1d0ca
danil <danil@DESKTOP-AHFQMQ9>
2025-02-14 01:32:23 +0500

    Разработанный пользовательский элемент User. Фамилия

---
+++ Pages/Items/User.xaml.cs
@@ -0,0 +1,41 @@
+﻿using _333.Classes.Common;
+using _333.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _333.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для User.xaml
+    /// </summary>
+    public partial class User : UserControl
+    {
+        Users user { get; set; }
+        Main main { get; set; }
+        public User(Users user, Main main)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.main = main;
+            imgUser.Source = BitmapFromArrayByte.LoadImage(user.Photo);
+            FIO.Content = user.ToFIO();
+        }
+
+        private void SelectChat(object sender, MouseButtonEventArgs e)
+        {
+            main.SelectUser(user);
+        }
+    }
+}
